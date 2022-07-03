f13ca4f45a22fa77f98e0e04593c88a78afc395a
wahidabd <wahed.blog99@gmail.com>
2022-07-03 23:00:20 +0700

    add: dependecy, theme and color

--- app/src/main/java/com/wahidabd/moviecompose/ui/theme/Color.kt
+++ app/src/main/java/com/wahidabd/moviecompose/ui/theme/Color.kt
@@ -2,7 +2,10 @@ package com.wahidabd.moviecompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val purple200 = Color(0xFF651FFF)
+val purple500 = Color(0xFF6200EA)
+val background = Color(0xFF2B292B)
+val background800 = Color(0xFF424242)
+val background900 = Color(0xFF212121)
+val white87 = Color(0Xddffffff)
+val shimmerHighLight = Color(0xA3C2C2C2)
--- app/src/main/java/com/wahidabd/moviecompose/ui/theme/Type.kt
+++ app/src/main/java/com/wahidabd/moviecompose/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.wahidabd.moviecompose.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -25,4 +26,60 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+)
+
+// set of dark material typography styles to start with.
+val DarkTypography = Typography(
+    h1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        color = Color.White,
+        fontSize = 28.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        color = Color.White,
+        fontSize = 21.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        color = Color.White,
+        fontSize = 14.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        color = white87,
+        fontSize = 14.sp
+    )
+)
+
+// set of light material typography styles to start with.
+val LightTypography = Typography(
+    h1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        color = background900,
+        fontSize = 28.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        color = background900,
+        fontSize = 21.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        color = background800,
+        fontSize = 14.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        color = background800,
+        fontSize = 14.sp
+    )
 )
